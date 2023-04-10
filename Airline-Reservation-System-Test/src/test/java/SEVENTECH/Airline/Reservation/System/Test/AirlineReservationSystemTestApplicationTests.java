83447d81e773646e6d5a6c9083d654cc7f5edcb2
Kaylee Groves <kayleegroves678@gmail.com>
2023-04-10 14:05:12 -0700

    spring boot project

---
+++ Airline-Reservation-System-Test/src/test/java/SEVENTECH/Airline/Reservation/System/Test/AirlineReservationSystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package SEVENTECH.Airline.Reservation.System.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationSystemTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
